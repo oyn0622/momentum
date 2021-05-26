35220b7e613c2fa2eafafb9a78e9af2ad3cf4440
oyn0622 <45144104+oyn0622@users.noreply.github.com>
2021-05-26 17:56:27 +0900

    Update clock.js

--- clock.js
+++ clock.js
@@ -2,19 +2,23 @@ const clockContainer = document.querySelector(".js-clock");
 const clockTitle = clockContainer.querySelector("h1");
 
 function getTime(){
+//new Date() : 현재 날짜와 시간을 가지는 인스턴스를 반환
     const date = new Date();
     const minutes= date.getMinutes();
     const hours= date.getHours();
     const seconds=date.getSeconds();
 
+//시간, 분, 초가 10이하의 숫자일 때 앞에 0을 붙여 표시 (삼항연산자 사용)
     clockTitle.innerText=`${hours<10 ? `0${hours}`:hours}:${
         minutes<10?`0${minutes}`:minutes
     }:${seconds<10 ? `0${seconds}`:seconds}`;
+
 
 }
 
 function init(){
     getTime();
+//1000=1초 (1초마다 새로고침)
     setInterval(getTime,1000);
 }
 
